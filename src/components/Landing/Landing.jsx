2de9c9524b81d611da9e82f462dd43da1618838e
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-11-07 22:19:49 -0600

    Created landing page mobile skeleton

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,5 +1,10 @@
 import logo from '../../Assets/Logo.svg';
-import heroCampMobile from '../../Assets/Hero Image (Tablet and Mobile).jpg'
+import heroCampMobile from '../../Assets/Hero Image (Tablet and Mobile).jpg';
+import airbnbLogo from '../../Assets/Airbnb.svg';
+import bookingLogo from '../../Assets/Booking.svg';
+import plumguideLogo from '../../Assets/Plum Guide.svg';
+import checkmark from '../../Assets/Checkmark.svg';
+
 
 const Landing = () => {
     return (
@@ -13,7 +18,26 @@ const Landing = () => {
         </div>
 
         <main className='main-container'>
-            <h1>Explore the best camps on Earth.</h1>
+            <h1 id='motto'>Explore the best camps on Earth.</h1>
+            <h4 id='description'>YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.</h4>
+            <ul>
+                <li><img src={checkmark} /><h4>Add your own camp suggestions.</h4></li>
+                <li><img src={checkmark} /><h4>Leave reviews and experiences.</h4></li>
+                <li><img src={checkmark} /><h4>See locations for all camps.</h4></li>
+            </ul>
+            <button className='view-campgrounds'>
+                View Campgrounds
+            </button>
+            <h4>Partnered with:</h4>
+
+            <div className='partners'>
+                <ul>
+                    <li><img src={airbnbLogo} alt='airbnb-logo' id='airbnb-logo' className='logo' /></li>
+                    <li><img src={bookingLogo} alt='booking-logo' id='booking-logo' className='logo' /></li>
+                    <li><img src={plumguideLogo} alt='plumguide-logo' id='plumguide-logo' className='logo' /></li>
+                </ul>
+            </div>
+
         </main>
     </>
     )
