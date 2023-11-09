c17404827b255d5be64cb6b6b1328232ee5f7a72
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-11-08 19:23:56 -0600

    Added react router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 import Landing from '../Landing/Landing';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+]);
 
 function App() {
 
   return (
     <>
-      <Landing />
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,43 +3,49 @@ import heroCampMobile from '../../Assets/Hero Image (Tablet and Mobile).jpg';
 import airbnbLogo from '../../Assets/Airbnb.svg';
 import bookingLogo from '../../Assets/Booking.svg';
 import plumguideLogo from '../../Assets/Plum Guide.svg';
+import heroCampDesktop from '../../Assets/Hero Image.jpg'
 import './Landing.css';
 
 
 const Landing = () => {
     return (
-    <>
-        <header className='logo'>
-            <img src={logo} alt='logo' />
-        </header>
+    <div className='landing-container'>
+        <div className='left'>
+            <header className='logo'>
+                <img src={logo} alt='logo' />
+            </header>
 
-        <div className='hero-camp-mobile-container'>
-            <img src={heroCampMobile} alt='Hero Camp' />
-        </div>
-
-        <main className='main-container'>
-            <h1 id='motto'>Explore the best camps on Earth.</h1>
-            <h4 id='description'>YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.</h4>
-            <ul className='actions-list'>
-                <li className='actions'><h4>Add your own camp suggestions.</h4></li>
-                <li className='actions'><h4>Leave reviews and experiences.</h4></li>
-                <li className='actions'><h4>See locations for all camps.</h4></li>
-            </ul>
-            <button className='view-campgrounds'>
-                View Campgrounds
-            </button>
-            
-            <div className='partners'>
-            <h4>Partnered with:</h4>
-                <ul>
-                    <li><img src={airbnbLogo} alt='airbnb-logo' id='airbnb-logo' className='partner-logo' /></li>
-                    <li><img src={bookingLogo} alt='booking-logo' id='booking-logo' className='partner-logo' /></li>
-                    <li><img src={plumguideLogo} alt='plumguide-logo' id='plumguide-logo' className='partner-logo' /></li>
-                </ul>
+            <div className='hero-camp-mobile-container'>
+                <img src={heroCampMobile} alt='Hero Camp' />
             </div>
 
-        </main>
-    </>
+            <main className='main-container'>
+                <h1 id='motto'>Explore the best camps on Earth.</h1>
+                <h4 id='description'>YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.</h4>
+                <ul className='actions-list'>
+                    <li className='actions'><h4>Add your own camp suggestions.</h4></li>
+                    <li className='actions'><h4>Leave reviews and experiences.</h4></li>
+                    <li className='actions'><h4>See locations for all camps.</h4></li>
+                </ul>
+                <button className='view-campgrounds'>
+                    View Campgrounds
+                </button>
+                
+                <div className='partners'>
+                <h4>Partnered with:</h4>
+                    <ul>
+                        <li><img src={airbnbLogo} alt='airbnb-logo' id='airbnb-logo' className='partner-logo' /></li>
+                        <li><img src={bookingLogo} alt='booking-logo' id='booking-logo' className='partner-logo' /></li>
+                        <li><img src={plumguideLogo} alt='plumguide-logo' id='plumguide-logo' className='partner-logo' /></li>
+                    </ul>
+                </div>
+            </main>
+        </div>
+
+        <div className='hero-camp-desktop-container'>
+            <img src={heroCampDesktop} alt='Hero Camp' />
+        </div>
+    </div>
     )
 }
 
