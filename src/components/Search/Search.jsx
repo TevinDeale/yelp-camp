5da9b9a0e56b4ade2c492972fec796d2e3ac728b
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-11-11 10:12:34 -0600

    added search functionality

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,6 +2,7 @@ import "./Search.css";
 import Navbar from "../Static/Navbar";
 import Footer from "../Static/Footer";
 import compressedImages from "../Static/campsites";
+import { useEffect, useState } from "react";
 
 const Card = (props) => {
 
@@ -21,6 +22,21 @@ const Card = (props) => {
 
 
 const Search = () => {
+
+    const [camps, setCamps] = useState(compressedImages);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = () => {
+        console.log(searchTerm);
+        if (searchTerm) {
+            setCamps(compressedImages.filter(camp => camp.name.toLowerCase().includes(searchTerm.toLowerCase())));
+        } else {
+            setCamps(compressedImages);
+        }
+    }
+
+
+
     return (
     <div className="search-page-container">
         <Navbar />
@@ -29,19 +45,23 @@ const Search = () => {
             <h1>Welcome to YelpCamp!</h1>
             <h4>View our hand-picked campgrounds from all over the world, or add your own.</h4>
             <form onSubmit={(e) => e.preventDefault()}>
-                <input type="text" placeholder="Search for camps"></input>
+                <input onChange={(e) => {setSearchTerm(e.target.value)}} type="text" placeholder="Search for camps"></input>
                 
                 <br />
-                <button>Search</button>
+                <button onClick={handleSearch}>Search</button>
                 <h4 id='add-campground'>Or add your own campground</h4>
             </form>
         </main>
-        <div className="search-container">
-            {compressedImages.map((camp) => {
-                return (
-                <Card name={camp.name} desciption={camp.desciption} img={camp.img}/>
-                )
-            })}
+        <div className="search-container-no-camps">
+            {
+                camps.length === 0 ? <h4 id='no-camps'>There no camps that match your search.</h4> : <div className="search-container">
+                {camps.map((camp) => {
+                    return (
+                    <Card name={camp.name} desciption={camp.desciption} img={camp.img}/>
+                    )
+                })} </div>
+                
+            }
         </div>
 
         <Footer />
