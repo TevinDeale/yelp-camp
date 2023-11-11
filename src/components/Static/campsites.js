80d0568bb5b5f6e5b8317c52cb05f16c1252234f
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-11-11 09:32:55 -0600

    added Ids to the campsites data

--- src/components/Static/campsites.js
+++ src/components/Static/campsites.js
@@ -6,26 +6,32 @@ import onay from '../../Assets/Camp Images/Compressed Images/Onay Beach.jpg'
 import sevenSisters from '../../Assets/Camp Images/Compressed Images/Seven Sisters Waterfall.jpg'
 
 const compressedImages = [{
+    id: 1,
     name: 'Buloy Springs',
     desciption: 'Sandy Desert',
     img: buloy
 }, {
+    id: 2,
     name: 'Calaguas Island',
     desciption: 'A paradise of islands that can rival the white sand beauty of Boracay.',
     img: calaguas
 }, {
+    id: 3,
     name: 'Latik Riverside',
     desciption: 'Philippines is one of the most dazzling countries in all of Asia.',
     img: latik
 }, {
+    id: 4,
     name: 'Mount Ulap',
     desciption: 'One of the most famous hikes in Benguet is Mt Ulap in Itogon.',
     img: ulap
 }, {
+    id: 5,
     name: 'Onay Beach',
     desciption: 'This is one of the best beach camping sites, beautiful and pristine.',
     img: onay
 }, {
+    id: 6,
     name: 'Seven Sisters Waterfall',
     desciption: 'The Seven Sisters is the 39th tallest waterfall in Norway.',
     img: sevenSisters
